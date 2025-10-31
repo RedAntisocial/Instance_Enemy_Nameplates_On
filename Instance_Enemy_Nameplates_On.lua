689c5eb589cb477f13c0bdc38e17f3f1073e0856
Alex Pierce <48069006+RedAntisocial@users.noreply.github.com>
2025-10-31 19:17:19 -0400

    Initial load onto GitHub
    Might as well put this here.

---
+++ Instance_Enemy_Nameplates_On.lua
@@ -0,0 +1,20 @@
+local IENOF = CreateFrame("Frame")
+
+-- Run on login and zone changes
+IENOF:RegisterEvent("PLAYER_ENTERING_WORLD")
+IENOF:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+IENOF:RegisterEvent("ZONE_CHANGED_INDOORS")
+IENOF:RegisterEvent("ZONE_CHANGED")
+
+local function IENO_UpdateNameplates()
+    local inInstance = IsInInstance()
+    if inInstance then
+        SetCVar("nameplateShowAll", 1)
+    else
+        SetCVar("nameplateShowAll", 0)
+    end
+end
+
+IENOF:SetScript("OnEvent", function(self, event, ...)
+    IENO_UpdateNameplates()
+end)
